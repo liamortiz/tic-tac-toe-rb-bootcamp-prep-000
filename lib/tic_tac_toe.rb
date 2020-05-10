ece6b34dae4bfa96e63bc8498914cf589f9feb7f
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-10 16:44:42 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,13 @@
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0, 1, 2], # Top row
+                    [3, 4, 5], # Bottom row
+                    [6, 7, 8], # Last row
+                    [0, 3, 6], # Left col
+                    [1, 4, 7], # Middle col
+                    [2, 5, 8], # Right
+                    [0, 4, 8], # X1
+                    [2, 4, 6]  # X2
+                    ]
+
+def display_board(board)
+end
