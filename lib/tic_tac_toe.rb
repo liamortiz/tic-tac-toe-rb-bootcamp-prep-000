db1dc087d76869da55ad8df2eac29a306243cdf8
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-10 16:48:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,9 +26,9 @@ def move(board, index, char)
 end
 
 def position_taken?(board, index)
-  !board[index] == ' '
+  !board[index] == " "
 end
 
 def valid_move?(board, index)
-  
+  index.between?(0, 8) and !position_taken?(board, index)
 end
